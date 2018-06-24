c5086f3771b12e3fd242d522a41e51a109ad35cd
thejaelal <jay.lal@somaiya.edu>
2018-06-24 11:29:14 +0530

    Camera Connection Established

--- app/src/main/java/lal/jay/camera2demo/MainActivity.java
+++ app/src/main/java/lal/jay/camera2demo/MainActivity.java
@@ -1,15 +1,20 @@
 package lal.jay.camera2demo;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.Build;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Size;
@@ -35,8 +40,9 @@ public class MainActivity extends AppCompatActivity {
             //width x height -> 1080 x 1860 for Redmi Note 4
 
             //Setup Camera now since the SurfaceTexture is available
-            setupCamera(width,height);
+            setupCamera(width, height);
             //Toast.makeText(getApplicationContext(),"SurfaceCameraId = "+cameraId,Toast.LENGTH_SHORT).show();
+            connectCamera();
         }
 
         @Override
@@ -64,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onOpened(@NonNull CameraDevice camera) {
             cameraDevice = camera;
+            Toast.makeText(getApplicationContext(),"Connected to Camera!",Toast.LENGTH_SHORT).show();
         }
 
         @Override
@@ -82,36 +89,32 @@ public class MainActivity extends AppCompatActivity {
     private String cameraId;
     private Size previewSize;
 
-    private void setupCamera(int width, int height)
-    {
+    private void setupCamera(int width, int height) {
         CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
 
-        try{
-            for(String camId: camManager.getCameraIdList())
-            {
+        try {
+            for (String camId : camManager.getCameraIdList()) {
                 CameraCharacteristics camChars = camManager.getCameraCharacteristics(camId);
 
-                if(camChars.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_BACK)
-                {
+                if (camChars.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_BACK) {
 
                     //Adjust Sensor to device orientation
                     int deviceOrientation = getWindowManager().getDefaultDisplay().getRotation();
-                    int totalRotation = sensorToDeviceRotation(camChars,deviceOrientation);
+                    int totalRotation = sensorToDeviceRotation(camChars, deviceOrientation);
 
                     boolean swapRotation = totalRotation == 90 || totalRotation == 270;
 
                     int rotatedWidth = width;
                     int rotatedHeight = height;
 
-                    if (swapRotation)
-                    {
+                    if (swapRotation) {
                         rotatedWidth = height;
                         rotatedHeight = width;
                     }
                     StreamConfigurationMap map = camChars.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
 
                     //preview Size is 1440 x 1080 for Redmi Note 4
-                    previewSize = chooseOptimalSize(map.getOutputSizes(SurfaceTexture.class),rotatedWidth,rotatedHeight);
+                    previewSize = chooseOptimalSize(map.getOutputSizes(SurfaceTexture.class), rotatedWidth, rotatedHeight);
 
                     //Setup the global cameraId to this camId
                     cameraId = camId;
@@ -120,7 +123,7 @@ public class MainActivity extends AppCompatActivity {
                 }
 
             }
-        } catch (CameraAccessException e ){
+        } catch (CameraAccessException e) {
             e.printStackTrace();
         }
 
@@ -133,16 +136,14 @@ public class MainActivity extends AppCompatActivity {
     private Handler bgThreadHandler;
 
 
-    private void startBgThread()
-    {
+    private void startBgThread() {
         bgThread = new HandlerThread("BackgroundThread");
         bgThread.start();
 
         bgThreadHandler = new Handler(bgThread.getLooper());
     }
 
-    private void stopBgThread()
-    {
+    private void stopBgThread() {
         bgThread.quitSafely();
 
         try {
@@ -159,15 +160,15 @@ public class MainActivity extends AppCompatActivity {
 
 
     private static SparseIntArray ORIENTATIONS = new SparseIntArray();
-    static{
-        ORIENTATIONS.append(Surface.ROTATION_0,0);
-        ORIENTATIONS.append(Surface.ROTATION_90,90);
-        ORIENTATIONS.append(Surface.ROTATION_180,180);
-        ORIENTATIONS.append(Surface.ROTATION_270,270);
+
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 0);
+        ORIENTATIONS.append(Surface.ROTATION_90, 90);
+        ORIENTATIONS.append(Surface.ROTATION_180, 180);
+        ORIENTATIONS.append(Surface.ROTATION_270, 270);
     }
 
-    private static int sensorToDeviceRotation(CameraCharacteristics cameraCharacteristics, int deviceOrientation)
-    {
+    private static int sensorToDeviceRotation(CameraCharacteristics cameraCharacteristics, int deviceOrientation) {
         int sensorOrientation = cameraCharacteristics.get(CameraCharacteristics.SENSOR_ORIENTATION);
 
         //convert deviceOrientation to degrees
@@ -178,33 +179,72 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private static class CompareSizeByArea implements Comparator<Size>
-    {
+    private static class CompareSizeByArea implements Comparator<Size> {
 
         @Override
-        public int compare(Size lhs, Size rhs)
-        {
+        public int compare(Size lhs, Size rhs) {
 
             return Long.signum((long) lhs.getWidth() * lhs.getHeight() /
                     (long) rhs.getWidth() * rhs.getHeight());
         }
     }
 
-    private static Size chooseOptimalSize(Size[] choices,int width, int height)
-    {
+    private static Size chooseOptimalSize(Size[] choices, int width, int height) {
         List<Size> bigEnough = new ArrayList<Size>();
-        for(Size option : choices)
-        {
-            if(option.getHeight() == option.getWidth() * height/width && option.getWidth() >= width && option.getHeight() >= height)
+        for (Size option : choices) {
+            if (option.getHeight() == option.getWidth() * height / width && option.getWidth() >= width && option.getHeight() >= height)
                 bigEnough.add(option);
 
         }
 
-        if(bigEnough.size() > 0 )
+        if (bigEnough.size() > 0)
             return Collections.min(bigEnough, new CompareSizeByArea());
         else
             return choices[0];
     }
+
+    private static final int REQUEST_CAMERA_PERMISSION_RESULT = 0;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_CAMERA_PERMISSION_RESULT)
+        {
+            if(grantResults[0] != PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(getApplicationContext(),"Why you reject the request man?\n App needs Camera Access",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void connectCamera() {
+        CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        try {
+            //For Android Marshmallow Onwards..
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                //If permission has been granted, Open Camera...
+                if(ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+                    camManager.openCamera(cameraId, cameraStateCallback, bgThreadHandler);
+
+                //Permission not granted...
+                else{
+
+                    if(shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
+                        Toast.makeText(this,"App requires Camera Permission!",Toast.LENGTH_SHORT).show();
+                    }
+                    requestPermissions(new String[] {Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION_RESULT);
+                }
+            }
+            else{
+                camManager.openCamera(cameraId, cameraStateCallback, bgThreadHandler);
+            }
+
+
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -229,6 +269,7 @@ public class MainActivity extends AppCompatActivity {
         else{
             setupCamera(textureView.getWidth(), textureView.getHeight());
 //            Toast.makeText(getApplicationContext(), "CameraId = " + cameraId, Toast.LENGTH_SHORT).show();
+            connectCamera();
         }
     }
 
